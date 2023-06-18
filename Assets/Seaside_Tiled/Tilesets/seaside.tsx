8f2d8077908ddef21d83a692019642a67c68f77e
Nick <nicholasastamatis@gmail.com>
2023-06-18 08:01:47 -0400

    Putting in Tiled project hierarchy in assets

---
+++ Assets/Seaside_Tiled/Tilesets/seaside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="seaside" tilewidth="16" tileheight="16" tilecount="5000" columns="50">
+ <image source="seaside_tileset_Nick.png" width="800" height="1600"/>
+</tileset>
