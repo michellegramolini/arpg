93072526e169cf9d7c34d9734e117964262d0a7e
Chufted <133513326+Chufted@users.noreply.github.com>
2023-06-18 13:34:24 -0400

    fixed tsx references, added .pngs
    -added height_map and terrain_map pngs
    - tested in unity SampleScene

--- Assets/Graphics/Tilesets/height_map.tsx
+++ Assets/Graphics/Tilesets/height_map.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="height_map" tilewidth="16" tileheight="16" tilecount="5" columns="1">
- <image source="../../../../../Tiled Test/height_map.png" width="16" height="80"/>
+ <image source="height_map.png" width="16" height="80"/>
  <tile id="0">
   <properties>
    <property name="height_value" type="int" value="0"/>
